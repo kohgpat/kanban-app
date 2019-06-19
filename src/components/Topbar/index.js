1aaf7ffbccbd0cca33b50b5cd97846795b37ab28
Nikolay Burlov <kohgpat@gmail.com>
2019-06-19 12:37:07 +0800

    Add current user avatar

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -4,7 +4,15 @@ import * as s from './styles';
 const Topbar = () => (
   <s.Topbar>
     <s.Logo />
+
     <s.Separator />
+
+    <s.Separator pullRight />
+
+    <s.CurrentUser>
+      <s.CurrentUserIcon />
+      <s.CurrentUserName>Leonetta Lloyd</s.CurrentUserName>
+    </s.CurrentUser>
   </s.Topbar>
 );
 
--- src/components/Topbar/styles.js
+++ src/components/Topbar/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import logoIcon from './images/logo.svg';
+import avatar from './images/avatar.png';
 
 export const Topbar = styled.div`
   display: flex;
@@ -25,4 +26,32 @@ export const Separator = styled.div`
   background-color: #F7F8F9;
   height: 36px;
   width: 3px;
+
+  ${props => props.pullRight && `
+    margin-left: auto;
+  `};
+`;
+
+export const CurrentUser = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 32px 0 28px;
+  height: 100%;
+`;
+
+export const CurrentUserIcon = styled.div`
+  height: 30px;
+  width: 30px;
+  background-image: url(${avatar});
+  background-position: center center;
+  background-size: cover;
+  border-radius: 50%;
+`;
+
+export const CurrentUserName = styled.span`
+  display: block;
+  font-size: 15px;
+  font-weight: 600;
+  line-height: 20px;
+  padding-left: 16px;
 `;
