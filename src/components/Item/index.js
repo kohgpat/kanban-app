7ddf0f91e55507601abe5809607d6d56325cfb23
Nikolay Burlov <kohgpat@gmail.com>
2019-06-21 15:37:58 +0800

    Remove obsolete import

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import DisplayItem from "./DisplayItem";
 import EditItem from "./EditItem";
-import * as s from "./styles";
 
 const Item = ({ item, ...restProps }) => {
   return item.isEditing ? (
