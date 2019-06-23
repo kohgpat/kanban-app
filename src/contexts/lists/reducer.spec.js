08ca55a8661964716ce28d57a4754835129cc200
Nikolay Burlov <kohgpat@gmail.com>
2019-06-23 19:27:13 +0800

    Add item save spec

--- src/contexts/lists/reducer.spec.js
+++ src/contexts/lists/reducer.spec.js
@@ -3,7 +3,8 @@ import {
   LISTS_ITEMS_MOVE_TO_LIST,
   LISTS_ITEMS_SET_FILTER,
   LISTS_ITEMS_ADD_ITEM,
-  LISTS_ITEMS_TOGGLE_EDIT
+  LISTS_ITEMS_TOGGLE_EDIT,
+  LISTS_ITEMS_SAVE
 } from "./actions";
 import reducer from "./reducer";
 
@@ -146,6 +147,38 @@ it("should toggle edit flag on item", () => {
   };
 
   expect(reducer(state, action).lists.items[1].isEditing).toEqual(true);
+});
+
+it("should update item on the list", () => {
+  const state = {
+    lists: {
+      lists: {
+        1: {
+          id: 1,
+          itemIds: [1]
+        }
+      },
+      items: {
+        1: {
+          id: 1,
+          isEditing: true
+        }
+      }
+    }
+  };
+
+  const action = {
+    type: LISTS_ITEMS_TOGGLE_EDIT,
+    item: {
+      ...state.lists.items[1],
+      text: "New text"
+    }
+  };
+
+  const newItem = reducer(state, action).lists.items[1];
+
+  expect(newItem.isEditing).toEqual(false);
+  expect(newItem.text).toEqual("New text");
 });
 
 it("should set filter", () => {
