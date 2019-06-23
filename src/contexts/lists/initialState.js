60188c7320df47e4a88d05a86dea234db57b6dca
Nikolay Burlov <kohgpat@gmail.com>
2019-06-23 18:34:54 +0800

    Change initialState for lists

--- src/contexts/lists/initialState.js
+++ src/contexts/lists/initialState.js
@@ -8,83 +8,37 @@ const initialState = {
     1: {
       id: 1,
       name: "Backlog",
-      itemIds: [1, 2, 3]
+      itemIds: [1]
     },
     2: {
       id: 2,
       name: "In progress",
-      itemIds: [4, 5, 6]
+      itemIds: [2]
     },
     3: {
       id: 3,
       name: "Done",
-      itemIds: [7, 8, 9]
+      itemIds: [3]
     }
   },
   listIds: [1, 2, 3],
   items: {
     1: {
       id: 1,
-      name: "Nulla volutpat aliquam velit",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
+      name: "Todo #1",
+      text: "Detailed description",
       updatedAt: new Date()
     },
     2: {
       id: 2,
-      name: "Facilisis in pretium nisl aliquet",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
+      name: "In progress #1",
+      text: "Detailed description",
       updatedAt: new Date()
     },
     3: {
       id: 3,
-      name: "Eget porttitor lorem",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
-
-      updatedAt: new Date()
-    },
-    4: {
-      id: 4,
-      name: "Eget porttitor lorem",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
-      updatedAt: new Date()
-    },
-    5: {
-      id: 5,
-      name: "Consectetur adipiscing elit",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
-      updatedAt: new Date()
-    },
-    6: {
-      id: 6,
-      name: "Nulla volutpat aliquam velit",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
-      updatedAt: new Date()
-    },
-    7: {
-      id: 7,
-      name: "Ac tristique libero volutpat at",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
-      updatedAt: new Date()
-    },
-    8: {
-      id: 8,
-      name: "Phasellus iaculis neque",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
-      updatedAt: new Date()
-    },
-    9: {
-      id: 9,
-      name: "Facilisis in pretium nisl aliquet",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat ar",
+      name: "Done #1",
+      text: "Detailed description",
       updatedAt: new Date()
     }
   }
